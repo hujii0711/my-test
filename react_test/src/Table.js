4691eee697112afae8c2c8ed2f575d78655e6505
hujii0711 <hujii0711@gmail.com>
2022-04-18 23:32:19 +0900

    리액트 수정

--- react_test/src/Table.js
+++ react_test/src/Table.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { memo } from 'react';
 import Tr from './Tr';
 
 const Table = ({onClick, tableData, dispatch, cellData}) => {
 
   return (
       <table onClick={onClick}>
-        {Array(tableData.length).fill().map((tr, i) => ( <Tr dispatch={dispatch} rowIndex={i} rowData={tableData[i]}/>))}
+        {Array(tableData.length).fill().map((tr, i) => ( 
+          //useMemo(
+          //  () => {
+              <Tr key={i} dispatch={dispatch} rowIndex={i} rowData={tableData[i]}/>
+          //  }, []
+          //)
+        ))}
       </table>
   );
 };
--- react_test/src/Tr.js
+++ react_test/src/Tr.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import Td from './Td';
 
-const Tr = ({rowData, rowIndex, dispatch}) => {
+const Tr = memo(({rowData, rowIndex, dispatch}) => {
 
   return (
     <tr>
-      {Array(rowData.length).fill().map((td, i) => (<Td dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>))}
+      {Array(rowData.length).fill().map((td, i) => (
+        //컴포넌트 자체를 기억하는 useMemo : 최후의 수단
+        //React Hook "useMemo" cannot be called inside a callback
+        //useMemo(
+        //  () => {
+            <Td key={i} dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>
+        //  },
+        //  [rowData[i]]
+        //)
+      ))}
     </tr>
   );
-};
+});
 export default Tr;
 
