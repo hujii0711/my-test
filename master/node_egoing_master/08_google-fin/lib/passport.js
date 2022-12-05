e8c630db77384c13e6ca25819880785d66fe5279
hujii0711 <hujii0711@gmail.com>
2022-12-05 17:42:06 +0900

    20221205

--- master/node_egoing_master/08_google-fin/lib/passport.js
+++ master/node_egoing_master/08_google-fin/lib/passport.js
@@ -1,92 +1,107 @@
-var db = require('../lib/db');
-var bcrypt = require('bcrypt');
-var shortid = require('shortid');
+var db = require("../lib/db");
+var bcrypt = require("bcrypt");
+var shortid = require("shortid");
 
 module.exports = function (app) {
-    var passport = require('passport'),
-        LocalStrategy = require('passport-local').Strategy,
-        GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+  var passport = require("passport"),
+    LocalStrategy = require("passport-local").Strategy,
+    GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
-    app.use(passport.initialize());
-    app.use(passport.session());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-    passport.serializeUser(function(user, done) {
-        console.log('serializeUser', user);
-        done(null, user.id);
-    });
-    passport.deserializeUser(function(id, done) {
-        var user = db.get('users').find({id:id}).value();
-        console.log('deserializeUser', id, user);
-        done(null, user);
-    });
+  passport.serializeUser(function (user, done) {
+    console.log("serializeUser", user);
+    done(null, user.id);
+  });
+  passport.deserializeUser(function (id, done) {
+    var user = db.get("users").find({ id: id }).value();
+    console.log("deserializeUser", id, user);
+    done(null, user);
+  });
 
-    passport.use(new LocalStrategy({
-            usernameField: 'email',
-            passwordField: 'pwd'
-        },
-        function(email, password, done) {
-            console.log('LocalStrategy', email, password);
-            var user = db.get('users').find({
-                email: email
-            }).value();
-            if (user) {
-                bcrypt.compare(password, user.password, function(err, result) {
-                    if(result) {
-                        return done(null, user, {
-                            message: 'Welcome.'
-                        });
-                    } else {
-                        return done(null, false, {
-                            message: 'Password is not correct.'
-                        });
-                    }
-                });
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "pwd",
+      },
+      function (email, password, done) {
+        console.log("LocalStrategy", email, password);
+        var user = db
+          .get("users")
+          .find({
+            email: email,
+          })
+          .value();
+        if (user) {
+          bcrypt.compare(password, user.password, function (err, result) {
+            if (result) {
+              return done(null, user, {
+                message: "Welcome.",
+              });
             } else {
-                return done(null, false, {
-                    message: 'There is no email.'
-                });
+              return done(null, false, {
+                message: "Password is not correct.",
+              });
             }
+          });
+        } else {
+          return done(null, false, {
+            message: "There is no email.",
+          });
         }
-    ));
+      }
+    )
+  );
 
-    var googleCredentials = require('../config/google.json');
-    passport.use(new GoogleStrategy({
-            clientID: googleCredentials.web.client_id,
-            clientSecret: googleCredentials.web.client_secret,
-            callbackURL: googleCredentials.web.redirect_uris[0]
-        },
-        function(accessToken, refreshToken, profile, done) {
-            console.log('GoogleStrategy', accessToken, refreshToken, profile);
-            var email = profile.emails[0].value;
-            var user = db.get('users').find({email:email}).value();
-                if(user) {
-                    user.googleId = profile.id;
-                    db.get('users').find({id:user.id}).assign(user).write();
-                } else {
-                user = {
-                    id:shortid.generate(),
-                    email:email,
-                    displayName:profile.displayName,
-                    googleId:profile.id
-                }
-                db.get('users').push(user).write();
-            }
-            done(null, user);
+  var googleCredentials = require("../config/google.json");
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: googleCredentials.web.client_id,
+        clientSecret: googleCredentials.web.client_secret,
+        callbackURL: googleCredentials.web.redirect_uris[0],
+      },
+      function (accessToken, refreshToken, profile, done) {
+        console.log("GoogleStrategy", accessToken, refreshToken, profile);
+        var email = profile.emails[0].value;
+        var user = db.get("users").find({ email: email }).value();
+        if (user) {
+          user.googleId = profile.id;
+          db.get("users").find({ id: user.id }).assign(user).write();
+        } else {
+          user = {
+            id: shortid.generate(),
+            email: email,
+            displayName: profile.displayName,
+            googleId: profile.id,
+          };
+          db.get("users").push(user).write();
         }
-    ));
-    app.get('/auth/google',
-        passport.authenticate('google', {
-            scope: ['https://www.googleapis.com/auth/plus.login', 'email']
-        }
-    ));
+        done(null, user);
+      }
+    )
+  );
 
-    app.get('/auth/google/callback',
-        passport.authenticate('google', {
-            failureRedirect: '/auth/login'
-        }),
-        function (req, res) {
-            res.redirect('/');
-        });
+  // 사용자가 Resource server로 인증을 하러갈 때 필요한 주소를 만들어주는 코드
+  // 인증과정을 거치는 과정을 거치는 구글 주소로 화면 리다이렉션 됨, 계정 선택 화면이 나옴
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["https://www.googleapis.com/auth/plus.login", "email"],
+    })
+  );
 
-    return passport;
-}
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", {
+      failureRedirect: "/auth/login",
+    }),
+    function (req, res) {
+      res.redirect("/");
+    }
+  );
+
+  return passport;
+};
