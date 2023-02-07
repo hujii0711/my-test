b1fd6a53dbdd94df67aa66deaf932b5931c16dfb
hujii0711 <hujii0711@gmail.com>
2023-02-07 13:51:21 +0900

    20230207

--- AWS/educational/websocket_sample/frontend/src/App.js
+++ AWS/educational/websocket_sample/frontend/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import axios from 'axios';
+import axios from "axios";
 import {
   MainContainer,
   ChatContainer,
@@ -8,27 +8,24 @@ import {
   Message,
   MessageInput,
 } from "@chatscope/chat-ui-kit-react";
-import { Map, List, Record } from 'immutable';
-import moment from 'moment';
+import { Map, List, Record } from "immutable";
+import moment from "moment";
 
-const API_GATEWAY_ID = "w2en10npqh"
-const SOCKET_API_GATEWAY_ID = "oxsz8n4mcj"
+const API_GATEWAY_ID = "w2en10npqh";
+const SOCKET_API_GATEWAY_ID = "oxsz8n4mcj";
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      data: Map(
-        {
-          messageList: [],
-          messages: []
-        })
-    }
+      data: Map({
+        messageList: [],
+        messages: [],
+      }),
+    };
     this.websocket = undefined;
     this.timer = undefined;
   }
   closeWebSocket = () => {
-
     if (this.timer) {
       clearInterval(this.timer);
       this.timer = null;
@@ -37,97 +34,93 @@ class App extends Component {
       this.websocket.close();
       this.websocket = null;
     }
-  }
+  };
   connectToWebScoket = () => {
-    const address = `wss://${SOCKET_API_GATEWAY_ID}.execute-api.ap-northeast-2.amazonaws.com/dev?user_id=test&room_id=test`
+    const address = `wss://${SOCKET_API_GATEWAY_ID}.execute-api.ap-northeast-2.amazonaws.com/dev?user_id=test&room_id=test`;
     this.websocket = new WebSocket(address);
 
     this.websocket.onopen = () => {
-
-      console.log("open")
+      console.log("open");
       this.timer = setInterval(() => {
-        this.websocket.send(JSON.stringify({ message: 'ping' }));
+        this.websocket.send(JSON.stringify({ message: "ping" }));
       }, 60 * 1000);
     };
 
     this.websocket.onmessage = (message) => {
-
       let obj = JSON.parse(message.data);
       this.onMessageReceived(obj);
-
-
-
     };
 
     this.websocket.onclose = (event) => {
-      console.log('onclose');
+      console.log("onclose");
       if (this.timer || this.websocket) this.closeWebSocket();
     };
 
     this.websocket.onerror = (event) => {
-      console.error('WebSocket error observed:', event);
+      console.error("WebSocket error observed:", event);
       if (this.timer || this.websocket) this.closeWebSocket();
     };
-  }
+  };
   componentDidMount = async () => {
     const { data } = this.state;
     const result = await axios({
-      method: 'GET',
+      method: "GET",
       url: `https://${API_GATEWAY_ID}.execute-api.ap-northeast-2.amazonaws.com/dev/chat`,
       params: {
-        room_id: "test"
-      }
-    });;
+        room_id: "test",
+      },
+    });
 
     this.setState({
-      data: data.set("messages", result.data).set("user_id", moment().valueOf())
-    })
+      data: data
+        .set("messages", result.data)
+        .set("user_id", moment().valueOf()),
+    });
 
     this.connectToWebScoket();
-  }
+  };
   onMessageReceived = async (message) => {
-    console.log(message)
+    console.log(message);
     if (message.timestamp) {
       const { data } = this.state;
       let list = data.get("messages");
-      list.push(message)
+      list.push(message);
       console.log(list);
       this.setState({
-        data: data.set("messages", list)
-      })
+        data: data.set("messages", list),
+      });
     }
-  }
+  };
   onSend = async (message) => {
     const { data } = this.state;
     const result = await axios({
-      method: 'PUT',
+      method: "PUT",
       url: `https://${API_GATEWAY_ID}.execute-api.ap-northeast-2.amazonaws.com/dev/chat`,
       data: {
         room_id: "test",
         text: message,
         user_id: data.get("user_id"),
-        name: "name_test"
-
-      }
-    });;
-  }
+        name: "name_test",
+      },
+    });
+  };
   getMessageList = () => {
-
     const { data } = this.state;
     const userId = data.get("user_id");
     let messageList = [];
     data.get("messages").forEach((message) => {
-      messageList.push(<Message
-        key={message.timestamp}
-        model={{
-          message: message.message,
-          sentTime: "just now",
-          sender: "Joe",
-          direction: (userId == message.user_id) ? "outgoing" : "not",
-        }}
-      />
+      messageList.push(
+        <Message
+          key={message.timestamp}
+          model={{
+            message: message.message,
+            sentTime: "just now",
+            sender: "Joe",
+            direction: userId == message.user_id ? "outgoing" : "not",
+          }}
+        />
       );
-    })
+    });
     // messageList.push(<Message
     //   model={{
     //     message: "test",
@@ -146,28 +139,24 @@ class App extends Component {
     //   }} />
 
     return messageList;
-
-  }
+  };
 
   render() {
-
     return (
-
       // Your JSX...
 
       <div style={{ position: "relative", height: "500px" }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
-              {this.getMessageList()}
-            </MessageList>
-            <MessageInput placeholder="Type message here" onSend={this.onSend} />
+            <MessageList>{this.getMessageList()}</MessageList>
+            <MessageInput
+              placeholder="Type message here"
+              onSend={this.onSend}
+            />
           </ChatContainer>
         </MainContainer>
       </div>
-
-    )
-
+    );
   }
 }
 
